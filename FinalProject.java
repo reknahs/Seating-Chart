becd12892856f063cf23df6d5c09b574663c80c9
Shanker <shankernram@gmail.com>
2022-05-05 15:14:14 -0700

    asdfasdf

--- FinalProject.java
+++ FinalProject.java
@@ -1,6 +1,5 @@
 public class FinalProject {
     public static void main(String[] args) {
         System.out.println("hi");
-        Classroom classroom = new Classroom(3);
     }
 }
--- SeatingChart.java
+++ SeatingChart.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
 public class SeatingChart {
-    public SeatingChart() {
-        
+
+    private boolean[][] classroom;
+    private ArrayList<Student> students;
+    private ArrayList<String> priorities;
+
+    public SeatingChart(boolean[][] classroom, ArrayList<Student> students, ArrayList<String> priorities) {
+        this.classroom = classroom;
+        this.students = students;
+        this.priorities = priorities; 
+
+        Student[][] order = getBestChart();
+    }
+
+    public Student[][] getBestChart() {
+        ArrayList<Student> newOrder;
+        for(int i = 0; i < priorities.size(); i++) {
+            newOrder = sort(newOrder(priorites[i]))
+        }
+        return new Student[][]{};
     }
 }
