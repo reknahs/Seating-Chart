ef7eed9b98d85089f0bb2014805c1c0271f8a211
Khadija Raza <khadijasupervised@gmail.com>
2022-05-05 14:23:46 -0800

    student bare

--- Student.java
+++ Student.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class Student {
     
+    private String name;
+    int grade;
+    boolean eyesight;
+    boolean hearing; 
+    ArrayList<Student> near;
+    ArrayList<Student> avoid;
+
+    public Student (String name, int grade, boolean eyesight, boolean hearing) {
+        this.name = name;
+        this.grade = grade;
+        this.eyesight = eyesight;
+        this.hearing = hearing;
+    }
+
+    
+
+
 }
