0762379adfca9a686ed6bbbe66bb4e3b53a3b570
Khadija Raza <khadijasupervised@gmail.com>
2022-05-09 13:22:34 -0800

    student

--- Student.java
+++ Student.java
@@ -2,21 +2,53 @@ import java.util.ArrayList;
 
 public class Student {
     
-    private String name;
-    int grade;
-    boolean eyesight;
-    boolean hearing; 
-    ArrayList<Student> near;
-    ArrayList<Student> avoid;
+    private String firstName;
+    private String lastName;
+    private int grade;
+    private boolean eyesight;
+    private boolean hearing; 
+    private ArrayList<Student> near;
+    private ArrayList<Student> avoid;
 
-    public Student (String name, int grade, boolean eyesight, boolean hearing) {
-        this.name = name;
+    public Student (String first, String last, int grade, boolean eyesight, boolean hearing) {
+        firstName = first;
+        lastName = last;
         this.grade = grade;
         this.eyesight = eyesight;
         this.hearing = hearing;
     }
 
+    public Student (String first, String last) {
+        firstName = first;
+        lastName = last;
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
     
+    public String getLastName () {
+        return lastName;
+    }
+
+    public boolean getEyesight () {
+        return eyesight;
+    }
+
+    public boolean getHearing () {
+        return hearing;
+    }
+
+    public ArrayList<Student> getNear() {
+        return near;
+    }
+    
+    public ArrayList<Student> getAvoid() {
+        return avoid;
+    }
+
+
+
 
 
 }
